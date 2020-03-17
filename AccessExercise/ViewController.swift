73a8e47863e11df308535a21d6b1a793e0870470
Stan <ck292704@gmail.com>
2020-03-17 10:15:30 +0800

    Create the project & edit gitignore file.

---
+++ AccessExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessExercise
+//
+//  Created by APPLE on 2020/3/17.
+//  Copyright © 2020 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
